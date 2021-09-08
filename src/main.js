5ce05930577d44987581af2c621dfd4e326996b2
Target-xhr <1642107938@qq.com>
2021-09-08 11:31:33 +0800

    第二版

--- src/main.js
+++ src/main.js
@@ -1,25 +1,14 @@
 import Vue from 'vue'
-import App from './App'
-import router from './router'
-
+import App from './App.vue'
+import router from './router/index'
+import store from './store'
+import less from 'less'
+Vue.use(less)
 Vue.config.productionTip = false
-import VueAwesomeSwiper from 'vue-awesome-swiper'
-import 'swiper/swiper-bundle.css'
-import { Swipe, SwipeItem } from 'vant';
-import { Lazyload } from 'vant';
-import Vant from 'vant';
-import 'vant/lib/index.css';
 
-
-
-Vue.use(Vant);
-Vue.use(Lazyload);
-Vue.use(Swipe);
-Vue.use(SwipeItem);
-Vue.use(VueAwesomeSwiper)
-/* eslint-disable no-new */
 new Vue({
-  el: '#app',
+  router,
+  store,
   router,
   render: h => h(App)
-})
+}).$mount('#app')
