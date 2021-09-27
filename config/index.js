1c7f3c854be4bc86fb7e6f420df7f342b686ba87
Target-xhr <1642107938@qq.com>
2021-09-27 11:40:07 +0800

    1

--- config/index.js
+++ config/index.js
@@ -12,7 +12,7 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       '/api': {
-        target: 'http://news.4399.com/gonglue/seer/tujian/948256.htm',//设置你调用的接口域名和端口号 别忘了加http
+        target: '',//设置你调用的接口域名和端口号 别忘了加http
         changeOrigin: true,//允许跨域
         pathRewrite: {
           '^/api': '' //这个是定义要访问的路径，名字随便写 
